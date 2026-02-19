e208f371a2c94719e43b17be313ac6cee1a9800c
Riddhi Mahajan <150167458+riddhuu@users.noreply.github.com>
2026-02-19 17:54:13 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using ContactListApplication_RM.Views;
+
+namespace ContactListApplication_RM;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new NavigationPage(new ContactsPage());
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactListApplication_RM
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
