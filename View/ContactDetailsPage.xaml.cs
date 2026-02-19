9426e4ab2a8decc1509c9ecf3aca8ce1fd2fe6e6
Riddhi Mahajan <150167458+riddhuu@users.noreply.github.com>
2026-02-19 17:56:50 -0500

    Add files via upload

---
+++ View/ContactDetailsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ContactListApplication_RM.Models;
+
+namespace ContactListApplication_RM.Views;
+
+public partial class ContactDetailsPage : ContentPage
+{
+    private readonly Models.Contact _selected;
+
+    public ContactDetailsPage(Models.Contact selected)
+    {
+        InitializeComponent();
+        _selected = selected;
+        BindingContext = _selected;
+    }
+
+    private async void OnBackClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+}
---
+++ View/ContactsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using ContactListApplication_RM.Models;
+using ContactListApplication_RM.ViewModels;
+
+namespace ContactListApplication_RM.Views;
+
+public partial class ContactsPage : ContentPage
+{
+    public ContactsPage()
+    {
+        InitializeComponent();
+        BindingContext = new ContactsViewModel();
+    }
+
+    private async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var selected = e.CurrentSelection?.FirstOrDefault() as Models.Contact;
+        if (selected is null)
+            return;
+
+        await Navigation.PushAsync(new ContactDetailsPage(selected));
+
+        ((CollectionView)sender).SelectedItem = null;
+    }
+}
+
